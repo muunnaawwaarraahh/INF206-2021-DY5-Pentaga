db39ab0c1602832eefef35190d268b18a01e3d1e
Munawarah <munawarah1@mhs.unsyiah.ac.id>
2021-05-05 22:09:11 +0700

    mengubah file functions.php agar terkoneksi ke database

--- functions.php
+++ functions.php
@@ -4,53 +4,53 @@
 $conn = mysqli_connect("localhost", "root", "", "pentaga");
 
 
-function query($query) {
-	global $conn;
-	$result = mysqli_query($conn, $query);
-	$rows = [];
-	while( $row = mysqli_fetch_assoc($result) ) {
-		$rows[] = $row;
-	}
-	return $rows;
-}
+// function query($query) {
+// 	global $conn;
+// 	$result = mysqli_query($conn, $query);
+// 	$rows = [];
+// 	while( $row = mysqli_fetch_assoc($result) ) {
+// 		$rows[] = $row;
+// 	}
+// 	return $rows;
+// }
 
-function registrasi($data) {
-	global $conn;
+// function registrasi($data) {
+// 	global $conn;
 
-	$namaLengkap = stripcslashes($data["namaLengkap"]);
-	$noKK = $data["noKK"];
-	$password = mysqli_real_escape_string($conn, $data["password"]);
-	$password2 = mysqli_real_escape_string($conn, $data["password2"]);
-
-
-	// cek username sudah ada atau belum
-	$result = mysqli_query($conn, "SELECT noKK FROM user WHERE noKK = '$noKK'");
-
-	if( mysqli_fetch_assoc($result) ) {
-		echo "<script>
-				alert('username sudah terdaftar!')
-		      </script>";
-		return false;
-	}
+// 	$namaLengkap = stripcslashes($data["namaLengkap"]);
+// 	$noKK = $data["noKK"];
+// 	$password = mysqli_real_escape_string($conn, $data["password"]);
+// 	$password2 = mysqli_real_escape_string($conn, $data["password2"]);
 
 
-	// cek konfirmasi password
-	if( $password !== $password2 ) {
-		echo "<script>
-				alert('konfirmasi password tidak sesuai!');
-		      </script>";
-		return false;
-	}
+// 	// cek username sudah ada atau belum
+// 	$result = mysqli_query($conn, "SELECT noKK FROM user WHERE noKK = '$noKK'");
 
-	// enkripsi password
-	$password = password_hash($password, PASSWORD_DEFAULT);
+// 	if( mysqli_fetch_assoc($result) ) {
+// 		echo "<script>
+// 				alert('username sudah terdaftar!')
+// 		      </script>";
+// 		return false;
+// 	}
 
-	// tambahkan userbaru ke database
-	mysqli_query($conn, "INSERT INTO user VALUES('', '$noKK', '$password', '$namaLengkap')");
 
-	return mysqli_affected_rows($conn);
+// 	// cek konfirmasi password
+// 	if( $password !== $password2 ) {
+// 		echo "<script>
+// 				alert('konfirmasi password tidak sesuai!');
+// 		      </script>";
+// 		return false;
+// 	}
 
-}
+// 	// enkripsi password
+// 	$password = password_hash($password, PASSWORD_DEFAULT);
+
+// 	// tambahkan userbaru ke database
+// 	mysqli_query($conn, "INSERT INTO user VALUES('', '$noKK', '$password', '$namaLengkap')");
+
+// 	return mysqli_affected_rows($conn);
+
+// }
 
 
 
